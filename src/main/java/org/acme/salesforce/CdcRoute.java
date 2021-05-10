5ac321911612ee37861e71ceadcbbf3f50e3838a
Marat Gubaidullin <marat.gubaidullin@gmail.com>
2021-05-10 17:24:37 -0400

    add headers to body

--- src/main/java/org/acme/salesforce/CdcRoute.java
+++ src/main/java/org/acme/salesforce/CdcRoute.java
@@ -2,12 +2,19 @@ package org.acme.salesforce;
 
 import org.apache.camel.builder.endpoint.EndpointRouteBuilder;
 
+import java.util.Map;
+
 public class CdcRoute extends EndpointRouteBuilder {
 
     @Override
     public void configure() throws Exception {
 
         from(salesforce("data/ChangeEvents").replayId(-1L))
+                .process(exchange -> {
+                    Map headers = exchange.getIn().getHeaders();
+                    Map body = exchange.getIn().getBody(Map.class);
+                    body.putAll(headers);
+                })
                 .setHeader("topic", simple("${header.CamelSalesforceEntityName.toLowerCase()}"))
                 .toD(kafka("${header.topic}"));
     }
